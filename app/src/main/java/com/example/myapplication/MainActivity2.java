5bbd6ac2043cdf367e68f03e5544b76bbc541316
Kane Kirwan <kanekirwan809@gmail.com>
2025-03-28 12:18:33 +0000

    .

--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -1,6 +1,5 @@
 package com.example.myapplication;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,24 +10,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    Button screen2;
+public class MainActivity2 extends AppCompatActivity implements View.OnClickListener {
+    Button screen1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        screen2 = findViewById(R.id.button);
-        screen2.setOnClickListener(this);
+        setContentView(R.layout.activity_main2);
+        screen1 = findViewById(R.id.button2);
+        screen1.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        switchActivity();
-    }
-
-    private void switchActivity(){
-        Intent switchActivityIntent = new Intent(this, MainActivity2.class);
-        startActivity(switchActivityIntent);
+        finish();
     }
 }
