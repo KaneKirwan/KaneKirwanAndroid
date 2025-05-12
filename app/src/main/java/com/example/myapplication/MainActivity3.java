2cd9ae8d7de83fb60b1e4e33072f2ac30653855e
kanekirwan <kanekirwan809@gmail.com>
2025-05-12 14:34:24 +0100

    .

--- app/src/main/java/com/example/myapplication/MainActivity3.java
+++ app/src/main/java/com/example/myapplication/MainActivity3.java
@@ -1,16 +1,13 @@
 package com.example.myapplication;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
 public class MainActivity3 extends AppCompatActivity implements View.OnClickListener {
     Button screen2;
     Button add_button;
@@ -23,42 +20,30 @@ public class MainActivity3 extends AppCompatActivity implements View.OnClickList
         screen2 = findViewById(R.id.button3);
         screen2.setOnClickListener(this);
         EdgeToEdge.enable(this);
-
-
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-
         add_button = findViewById(R.id.add_button);
         minus_button = findViewById(R.id.minus_button);
         count_val = findViewById(R.id.count_val);
-
-
         add_button.setOnClickListener(this);
         minus_button.setOnClickListener(this);
     }
-
     @Override
     public void onClick(View v) {
-        finish();
-
         int current_val = Integer.parseInt(count_val.getText().toString());
         int new_val = current_val;
-
-
-        if (v.getId() == R.id.add_button){
-            new_val = current_val +1;
-
-
-        } else if (v.getId() == R.id.minus_button) {
-            new_val = current_val -1;
+        if (v.getId() == R.id.add_button) {
+            new_val = current_val + 1;
+        }
+        else if (v.getId() == R.id.minus_button) {
+            new_val = current_val - 1;
+        }
+        else if (v.getId() == R.id.button3) {
+            finish();
         }
         count_val.setText(String.valueOf(new_val));
     }
-
-
 }
